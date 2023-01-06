c078beeee36ddd3e75021b38f8ab16a24b9e397d
Sebastian Moser <sewi.moser@gmail.com>
2023-01-06 05:29:56 +0100

    commiting, so that i can run the fresh version directly from github with npx github:sewimoser/...

--- create-render.js
+++ create-render.js
@@ -7,13 +7,13 @@ const working_dir = process.cwd()
 const file_dir = __dirname
 
 //check if dir alreay exists
-if (fs.existsSync(working_dir + project_name)){
+if (fs.existsSync(working_dir + "/" + project_name)){
 	console.log("There already exists a folder with the name " + project_name)
 	process.exit()
 }
 
 
-fs.mkdirSync(working_dir + project_name)
+fs.mkdirSync(working_dir + "/" + project_name)
 
 
 
