47082b00ec85a729e2db5701aacdeaf2ee15e72d
Sebastian Moser <sewi.moser@gmail.com>
2023-01-06 06:03:13 +0100

    commiting, so that i can run the fresh version directly from github with npx github:sewimoser/...

--- create-render.js
+++ create-render.js
@@ -24,10 +24,10 @@ fs.mkdirSync(project_dir)
 process.chdir(project_dir)
 
 //yarn init
-exec("yarn init -y")
+const init_log = exec("yarn init -y")
 
 //copy over the template
-exec("cp -r " + file_dir + "/template/* " + project_dir)
+const copy_log = exec("cp -r " + file_dir + "/template/* " + project_dir)
 
 //package.json
 const package_json = `{
@@ -41,6 +41,7 @@ const package_json = `{
 }`
 
 fs.writeFileSync(project_dir + "/package.json", package_json)
+fs.mkdirSync(project_dir + "/public")
 
 //mize.toml
 const mize_toml = `
@@ -54,8 +55,24 @@ webroot = "public"
 fs.writeFileSync(project_dir + "/mize.toml", mize_toml)
 
 //yarn add
-exec("yarn add -D @babel/core @babel/preset-env babel-loader eslint eslint-config-airbnb-base react-tracked webpack webpack-cli @babel/plugin-transform-runtime @babel/preset-react")
+const dev_deb_log = exec("yarn add -D @babel/core @babel/preset-env babel-loader eslint eslint-config-airbnb-base react-tracked webpack webpack-cli @babel/plugin-transform-runtime @babel/preset-react")
 
-exec("yarn add react react-dom")
+const deb_log = exec("yarn add react react-dom")
 
 
+//log all the data from all the exec's
+dev_deb_log.stdout.on('data', function(data) {
+	    console.log(data); 
+});
+
+deb_log.stdout.on('data', function(data) {
+	    console.log(data); 
+});
+
+init_log.stdout.on('data', function(data) {
+	    console.log(data); 
+});
+
+copy_log.stdout.on('data', function(data) {
+	    console.log(data); 
+});
